aabc42487c59df70e6e65f6a7265dcb99f539691
VIviane Dias <viviane.dias@colab.re>
2019-07-14 23:18:55 -0300

    Login logic is correct and working

--- routes/api/users.js
+++ routes/api/users.js
@@ -42,4 +42,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    // Find user by email
+    User.findOne({ email }).then(user => {
+      // Check for user
+      if (!user) {
+        return res.status(400).json({ msg: 'Email not found' });
+      }
+  
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          res.json({ msg: 'Success'})
+        } else {
+          return res.status(400).json({ msg: 'Password incorrect' });
+        }
+      });
+    });
+  });
+
 module.exports = router;
