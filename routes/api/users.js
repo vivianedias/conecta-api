a5557eaa520b7adc6b421fa8657c7825ceb4683d
VIviane Dias <viviane.dias@colab.re>
2019-07-14 23:05:12 -0300

    Can add users to database

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
+
+// Load User model
+const User = require('../../models/Users');
+
+// @route   GET api/users/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
-const posts = require('./routes/api/projects');
+const profile = require('./routes/api/profile');
+const projects = require('./routes/api/projects');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -19,7 +26,8 @@ app.get('/', (req, res) => res.send('Hello World'));
 
 // Use Routes
 app.use('/api/users', users);
-app.use('/api/projects', posts);
+app.use('/api/profile', profile);
+app.use('/api/projects', projects);
 
 // TODO: colocar isso em um .env 
 const port = process.env.PORT || 5000; 
