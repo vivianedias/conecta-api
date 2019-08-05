d411973cb939ca510718ee4b1d3b38f7b9d570f9
VIviane Dias <viviane.dias@colab.re>
2019-08-05 01:14:51 -0300

    Add sendgrid key to keys and package

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb+srv://abebe-conecta:c1o2n3e4c5t6a7@cluster0-0ig7l.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret'
+    secretOrKey: 'secret',
+    SENDGRID_API_KEY: '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'
 }
