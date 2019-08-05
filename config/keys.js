ef8793583b39e10a280d068e16274cc24525ef89
Viviane Dias <vivipirolad@gmail.com>
2019-08-05 09:23:35 -0300

    Delete keys because it exposes api keys

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://abebe-conecta:c1o2n3e4c5t6a7@cluster0-0ig7l.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret',
-    SENDGRID_API_KEY: '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'
-}
