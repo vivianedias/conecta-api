a8c9c33bff1f7105f90d2e73e26c9a3270a174f0
VIviane Dias <viviane.dias@colab.re>
2019-08-06 01:21:38 -0300

    New sendgrid key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb+srv://abebe-conecta:c1o2n3e4c5t6a7@cluster0-0ig7l.gcp.mongodb.net/test?retryWrites=true&w=majority',
     secretOrKey: 'secret',
-    SENDGRID_API_KEY: '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'
+    SENDGRID_API_KEY: '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'
 }
