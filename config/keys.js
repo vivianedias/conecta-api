f959b95de5b2f5f9267a4dac4de88026eed263ec
Viviane Dias <vivipirolad@gmail.com>
2019-08-06 01:23:50 -0300

    Delete keys.js again

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://abebe-conecta:c1o2n3e4c5t6a7@cluster0-0ig7l.gcp.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret',
-    SENDGRID_API_KEY: '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'
-}
