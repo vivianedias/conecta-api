d43d14d8a8e7bf1df9cdee78426d6b8f86bcaa5b
VIviane Dias <viviane.dias@colab.re>
2019-08-14 19:20:35 -0300

    Setting up env variables

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  sendgridApiKey: process.env.SENDGRID_API_KEY,
+  dbPassword: process.env.DB_PASSWORD
+}
--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const db = require('./config/db');
+const firebase = require('firebase');
 
 const users = require('./routes/api/users');
 const projects = require('./routes/api/projects');
 
 const app = express();
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxZweFnsN2gx3NAMULmzOskjepnX6eSgk",
+  authDomain: "abebe-conecta-3ad5e.firebaseapp.com",
+  databaseURL: "https://abebe-conecta-3ad5e.firebaseio.com",
+  projectId: "abebe-conecta-3ad5e",
+  storageBucket: "",
+  messagingSenderId: "131886864069",
+  appId: "1:131886864069:web:60d3e268965444c9"
+};
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,5 +45,6 @@ app.use('/api/projects', projects);
 const port = process.env.PORT || 5000; 
 
 app.use(express.static('public'));
+firebase.initializeApp(firebaseConfig);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
