566c9240822c7f629972912742dbbcf13213e25a
VIviane Dias <viviane.dias@colab.re>
2019-08-18 19:18:46 -0300

    Fix sytax error on server.js

--- server.js
+++ server.js
@@ -31,14 +31,14 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/projects', projects)
 
-// Serve static assets if in production
-if(process.env.NODE_ENV === 'production') {
-  // Set a static folder
-  app.user(express.static(client/build))
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-  app.get('*', (req, res => {
-    res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  }))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // TODO: colocar isso em um .env
