720c0e3aeaccd7c903cc85390aaf44afa9ef1024
VIviane Dias <viviane.dias@colab.re>
2019-08-23 09:17:02 -0300

    Trying build with client and server again

--- server.js
+++ server.js
@@ -41,6 +41,16 @@ require('./config/passport')(passport)
 app.use('/api/users', users)
 app.use('/api/projects', projects)
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // TODO: colocar isso em um .env
 const port = process.env.PORT || 5000
 
